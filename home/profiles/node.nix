0e72378c65a6da742010a1113125653f8089963d
Emilio Toledo <github@emiliotoledo.com>
2026-02-06 19:25:33 +0000

    fix npm overlay not being defined

--- home/profiles/node.nix
+++ home/profiles/node.nix
@@ -6,20 +6,20 @@
 }:
 
 let
-  # npmOverlay = final: prev: {
-  #   claude-max-api-proxy = prev.buildNpmPackage rec {
-  #     name = "claude-max-api-proxy";
+  npmOverlay = final: prev: {
+    # claude-max-api-proxy = prev.buildNpmPackage rec {
+    #   name = "claude-max-api-proxy";
 
-  #     src = prev.fetchFromGitHub {
-  #       owner = "atalovesyou";
-  #       repo = "claude-max-api-proxy";
-  #       rev = "main";
-  #       hash = "sha256-ypzeNoVIGTWhWng7vHWTBgL4/DTzBFX+4ljJ+dDipyA=";
-  #     };
+    #   src = prev.fetchFromGitHub {
+    #     owner = "atalovesyou";
+    #     repo = "claude-max-api-proxy";
+    #     rev = "main";
+    #     hash = "sha256-ypzeNoVIGTWhWng7vHWTBgL4/DTzBFX+4ljJ+dDipyA=";
+    #   };
 
-  #     npmDepsHash = "sha256-qS3YN6mwyMINyLRqD4ocEwhOoBAcfkjN79QVy+x5s2E=";
-  #   };
-  # };
+    #   npmDepsHash = "sha256-qS3YN6mwyMINyLRqD4ocEwhOoBAcfkjN79QVy+x5s2E=";
+    # };
+  };
 
   pkgsWithNpm = import pkgs.path {
     overlays = [ npmOverlay ];
